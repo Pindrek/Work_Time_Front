d49ab5704d50a1da1d3b58ad54678ba3969b46e4
pindrek <dimchoglo.mishelle@gmail.com>
2026-07-07 22:55:47 +0300

    new time format

--- Job_Time_Front/src/table.ts
+++ Job_Time_Front/src/table.ts
@@ -13,7 +13,7 @@ export async function DrawTable() {
         table.innerHTML += `
             <tr>
                 <th>${workTime.progress_bar}</th>
-                <th>${workTime.progress}</th>
+                <th>${TimeFormat(workTime.progress)}</th>
                 <th>${DateFormat(workTime.date.toString())}</th>
             </tr>
         `;
@@ -23,24 +23,17 @@ export async function DrawTable() {
 
 function DateFormat(date: string): string {
     let trueDate: string = '';
-    for (let i = 8; trueDate.length < 10;) {
-        trueDate += date[i]
-        if (i >= 5) {
-            if (i % 2 === 0 || i === 5) {
-                if (i === 6) i = -1;
-                i++;
-            }
-            else {
-                i -= 4;
-                trueDate += '.';
-            }
-        }
-        else {
-            i++;
-        }
-        if (trueDate.length === 5) {
-            trueDate += '.';
-        }
-    }
+    trueDate += date.slice(8, 10)
+    trueDate += '.';
+    trueDate += date.slice(5, 7)
+    trueDate += '.';
+    trueDate += date.slice(0, 4)
     return trueDate;
+}
+
+function TimeFormat(time: string): string {
+    let seconds: string = time.slice(10, 12).padStart(2, "0");
+    let minutes: string = time.slice(7, 9).padStart(2, "0");
+    let hours: string = time.slice(4, 6).padStart(2, "0");
+    return `${hours}:${minutes}:${seconds} / 06:00:00`;
 }
